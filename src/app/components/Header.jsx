19275471dd790e6d73daf0cfd52dd63f68537efd
Bibek Nayaju <bibeknayaju@github.com>
2024-05-13 16:14:10 +0545

    Add next-auth component and update Header component

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,8 +1,13 @@
+"use client";
+import { signIn, useSession, signOut } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { IoMdAddCircleOutline } from "react-icons/io";
 
 export default function Header() {
+  const { data: session } = useSession();
+  const [isOpen, setIsOpen] = React.useState(false);
   return (
     <div className="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
       <div className="flex justify-between items-center max-w-6xl mx-auto">
@@ -33,7 +38,26 @@ export default function Header() {
         />
 
         {/* for main section */}
-        <button className="text-sm font-semibold text-blue-500">Log In</button>
+        {session ? (
+          <div className="flex gap-2 items-center">
+            <IoMdAddCircleOutline
+              className="text-2xl cursor-pointer tranform hover:scale-125 transition duration-300 hover:text-red-600"
+              onClick={() => setIsOpen(true)}
+            />
+            <img
+              src={session.user.image}
+              alt={session.user.name}
+              className="h-10 w-10 rounded-full cursor-pointer"
+              onClick={signOut}
+            />
+          </div>
+        ) : (
+          <button
+            onClick={signIn}
+            className="text-sm font-semibold text-blue-500">
+            Log In
+          </button>
+        )}
       </div>
     </div>
   );
