34b022e4b46910ad7b921c9c5d16bb49ae47b607
Bibek Nayaju <bibeknayaju@github.com>
2024-05-14 09:12:18 +0545

    Update grid layout in Feed component

--- src/app/components/Feed.jsx
+++ src/app/components/Feed.jsx
@@ -6,12 +6,12 @@ export default function Feed() {
   return (
     <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
       {" "}
-      <section className="md:col-span-2">
+      <section className="md:col-span-3 lg:col-span-2">
         {/* left one for posts */}
         <Posts />
       </section>
       {/* right one for mini profile */}
-      <section className="hidden md:inline-grid md:col-span-1">
+      <section className="hidden md:hidden lg:inline-grid lg:col-span-1">
         <div className="fixed w-[380px]">
           {" "}
           <MiniProfile />
